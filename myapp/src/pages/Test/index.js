1e27749bca7f83b1e4c894861eca7b71b967ecb6
司豆豆 <1933320948@qq.com>
2023-11-11 23:10:20 +0800

    测试模板提交

--- myapp/src/pages/Test/index.js
+++ myapp/src/pages/Test/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import {getMainMode} from './api'
-import {Table,TextArea,Radio,CheckBox} from 'antd'
+import {Table,Radio,Checkbox,Input} from 'antd'
 import { Item } from 'rc-menu';
 import { getOpenCount } from 'rc-util/lib/PortalWrapper';
 import useToken from 'antd/es/theme/useToken';
+const { TextArea } = Input;
 require("./index.css") 
 
 
-
-class HandelWave extends Component {
+ 
+class Text extends Component {
 state = {
     update:false,
     ready:false,
@@ -19,21 +20,25 @@ state = {
             width:"25%",
             key:"name",
             render:(text,record)=>{
-                let content
+                let content = null
                 let child = record.isChildren
                 switch(record.typeValue){
                     case "Text": 
                         content = 
                             <>
+                                <div style={{ display: "flex", alignItems: "center" }}>
                                 {record.textValue+":"}
-                                <TextArea showCount maxLength={20}
-                                  defaultValue={this.getDefaultValue(record)[0]}
-                                  style={{marginLeft:"20px"}}
-                                  onChange={(e)=>{child? 
-                                    this.handleChildRadioChange(e,record):
-                                    this.handleFatherRadioChange(e,record)}}
+                                <TextArea
+                                    showCount maxLength={200}
+                                    defaultValue={this.getDefaultValue(record)[0]}
+                                    style={{marginLeft:"20px",width:"50%"}}
+                                    onChange={(e)=>{child? 
+                                        this.handleChildRadioChange(e,record):
+                                        this.handleFatherRadioChange(e,record)}}
                                 />
+                                </div>
                             </>
+                            
                         break
                     case "Radio":
                         content = <>
@@ -48,16 +53,19 @@ state = {
                                 </>
                                     
                         break
-                    case "CheckBox":
-                        <>
-                            {record.textValue+":"}  
-                            content = <CheckBox
-                            defaultValue={this.getDefaultValue(record)[0]}
-                            options={record.tabOptions}
-                            style={{marginLeft:"20px"}}
-                            // onChange = {}
-                            />
-                        </> 
+                    case "Checkbox": 
+                        const CheckboxGroup = Checkbox.Group;
+                        content =<>
+                                {record.textValue+":"}  
+                                <CheckboxGroup
+                                defaultValue={this.getDefaultValue(record)}
+                                options={record.tabOptions}
+                                style={{marginLeft:"20px"}}
+                                onChange={(checkedValues)=>{child? 
+                                    this.handleChildRadioChange(checkedValues,record):
+                                    this.handleFatherRadioChange(checkedValues,record)}}
+                                />
+                            </> 
                         break
                 }
                 return(content)
@@ -80,16 +88,34 @@ handleTextChange = (e,record)=>{
 
 }
 
+transLaterCheckboxTab(list){
+    const newList = []
+    list.map(item =>{
+        newList.push(item.value)
+    })
+    console.log("看下newList")
+    console.log(newList)
+    return newList
+}
+
 
 init = ()=>{
+    const saveData = []
+    const {dataSource} = this.state //模板数据
     //获取保存的数据，如果没有则重新生成并保存,如果有需要放初始值进 disPalyData 用于初始化数据
-    const {dataSource} = this.state
+
+    
     //获取模板
     getMainMode().then(data =>{
         let disPlayData = []
         let saveData = []
         //用于显示
         data.data.globalData.map(item =>{
+            // 如果保存过加入保存过的children
+            // const children = []
+            // saveData.filter(saveDataItem => saveDataItem.id === item.id)[0].value
+
+
             const newData = {
                 key:item.key,
                 id:item.id,
@@ -97,6 +123,7 @@ init = ()=>{
                 textValue:item.textValue,
                 typeValue:item.typeValue,
                 tabOptions:item.tabOptions
+
             }
             disPlayData.push(newData)
         })
@@ -142,7 +169,11 @@ getDefaultValue = (record)=>{
     const {saveData} = this.state
     if(record.isChildren){
         const targetData = saveData.filter(item => item.id === record.id.split("-")[0])[0]
-        const fatherValue = targetData.value[0]
+        const linkValue = record.linkValue[0]
+        const index = targetData.value.indexOf(linkValue)
+        //用于处理复选框  value有多个值，拿最后操作的哪个值
+        const length = targetData.value.length
+        const fatherValue = targetData.value[index]
         const childData =  targetData.children.filter(item => item.linkValue[0] === fatherValue)
         if(childData.length<=0){
             return []
@@ -157,16 +188,24 @@ getDefaultValue = (record)=>{
 
 
 handleFatherRadioChange = (e,record)=>{
-    //保存选中的值，进行child替换
-
-    const{saveData}  = this.state
-    this.setAttribute(record.id,{value:[e.target.value]},saveData,false)
-    //去除child
-    
     const {disPlayData,dataSource} = this.state
-    const value = e.target.value
+    console.log('调用父组件')
+    //保存选中的值，进行child替换
+    let value
+    const{saveData}  = this.state
+    if(record.typeValue === 'Checkbox'){
+        value = e
+        this.setAttribute(record.id,{value:e},saveData,false)
+    }else{
+        value = e.target.value
+        this.setAttribute(record.id,{value:[e.target.value]},saveData,false)
+    }
+   
+    console.log(saveData)
+    //去除child
+   
     const children = dataSource.filter(item => item.id === record.id)[0].children
-    const child = children.filter(item => item.linkValue[0] === value)[0]
+    const child = children.filter(item => value.includes(item.linkValue[0]))
     if(child === null || child === undefined){
         const displayFatherData = disPlayData.filter(item => item.id === record.id)[0]
         const otherDisPalyData = disPlayData.filter(item => item.id !== record.id)
@@ -187,7 +226,7 @@ handleFatherRadioChange = (e,record)=>{
     }
     const targetData = disPlayData.filter(item=> item.id === record.id)[0]
     const otherData = disPlayData.filter(item => item.id !== record.id)
-    const newChildren = [child]
+    const newChildren = [...child]
     console.log("看下child")
     console.log(child)
     const newTargetData = {
@@ -212,7 +251,13 @@ handleChildRadioChange = (e, record) => {
     console.log("看下id");
     console.log(record.id);
     const { saveData } = this.state;
-    this.setAttribute(record.id, {value: [e.target.value]}, saveData, false)
+    if(record.typeValue === 'Checkbox'){
+        this.setAttribute(record.id, {value: e}, saveData, false)
+
+    }else{
+        this.setAttribute(record.id, {value: [e.target.value]}, saveData, false)
+    }
+    
     console.log("看下saveData")
     console.log(this.state.saveData)
   };
@@ -301,4 +346,4 @@ setAttribute = (id, key, dataSource,isDelete) => {
   }
 
 }
-export default HandelWave;
+export default Text;
