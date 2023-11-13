19fb46309f0d02717e91d920e048710200eefc76
司豆豆 <1933320948@qq.com>
2023-11-13 16:14:05 +0800

    预切片函数测试

--- myapp/src/pages/label/label_core/index.js
+++ myapp/src/pages/label/label_core/index.js
@@ -37,6 +37,7 @@ class Waveform extends Component {
         })
       });
       this.setState({
+        //这里可能需要一个 加载完成的标志
         currentAudioBlob:data.currentAudioBlob
       })
     })
@@ -52,8 +53,8 @@ class Waveform extends Component {
     const wavesurfer = WaveSurfer.create({
       container,
       height:'130',
-      waveColor: '#12563e',
-      progressColor: '#bbb',
+      waveColor: 'rgb(200, 0, 200)',
+      progressColor: 'rgb(100, 0, 100)',
       autoCenter: false,
       plugins: [
         RegionsPlugin.create({
@@ -64,6 +65,27 @@ class Waveform extends Component {
         }),
       ],
     });
+
+    const re = wavesurfer.getActivePlugins('regions')
+
+    wavesurfer.on('decode', (duration) => {
+      const decodedData = ws.getDecodedData()
+      if (decodedData) {
+        const regions = extractRegions(decodedData.getChannelData(0), duration)
+    
+        // Add regions to the waveform
+        regions.forEach((region, index) => {
+          re.addRegion({
+            start: region.start,
+            end: region.end,
+            content: index.toString(),
+            drag: false,
+            resize: false,
+          })
+        })
+      }
+    })
+
 
     wavesurfer.on('finish', () => {
        this.setState({
@@ -149,6 +171,62 @@ class Waveform extends Component {
     this.clearLoop()
     waveSurfer.play(0)
   }
+
+
+  //预切片函数
+  extractRegions = (audioData, duration) => {
+  const minValue = 0.01
+  const minSilenceDuration = 0.1
+  const mergeDuration = 0.2
+  const scale = duration / audioData.length
+  const silentRegions = []
+
+  // Find all silent regions longer than minSilenceDuration
+  let start = 0
+  let end = 0
+  let isSilent = false
+  for (let i = 0; i < audioData.length; i++) {
+    if (audioData[i] < minValue) {
+      if (!isSilent) {
+        start = i
+        isSilent = true
+      }
+    } else if (isSilent) {
+      end = i
+      isSilent = false
+      if (scale * (end - start) > minSilenceDuration) {
+        silentRegions.push({
+          start: scale * start,
+          end: scale * end,
+        })
+      }
+    }
+  }
+
+  const mergedRegions = []
+  let lastRegion = null
+  for (let i = 0; i < silentRegions.length; i++) {
+    if (lastRegion && silentRegions[i].start - lastRegion.end < mergeDuration) {
+      lastRegion.end = silentRegions[i].end
+    } else {
+      lastRegion = silentRegions[i]
+      mergedRegions.push(lastRegion)
+    }
+  }
+
+  // Find regions that are not silent
+  const regions = []
+  let lastEnd = 0
+  for (let i = 0; i < mergedRegions.length; i++) {
+    regions.push({
+      start: lastEnd,
+      end: mergedRegions[i].start,
+    })
+    lastEnd = mergedRegions[i].end
+  }
+
+  return regions
+}
 
 
   handleStop = ()=>{
